0ff3990b794f6188c2182ef352563f29908bf193
surahutomo.pradana@bukalapak.com <surahutomo.pradana@bukalapak.com>
2023-11-25 02:54:19 +0700

    Add emulators

--- src/api/init.js
+++ src/api/init.js
@@ -1,28 +1,25 @@
+import { firebaseConfig } from "../firebase.config";
+
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBCLLQ_Qy5bfAwOOzLxiC0Y8nZB4R27a3k",
-  authDomain: "live-demo-firebase.firebaseapp.com",
-  projectId: "live-demo-firebase",
-  storageBucket: "live-demo-firebase.appspot.com",
-  messagingSenderId: "89267697560",
-  appId: "1:89267697560:web:b5180668bed6be7a0d9ded",
-  measurementId: "G-TYFCHQ4DYH"
-};
-
 const initializeFirestore = (app) => {
-  let firestore = getFirestore(app);
-  return firestore;
+  let firebaseStorage = getFirestore(app);
+  if (isLocalhost()) {
+    connectFirestoreEmulator(firebaseStorage, "localhost", 8080);
+  }
+  return firebaseStorage;
 };
 
 const initializeFirebaseStorage = (app) => {
   let firebaseStorage = getStorage(app);
+  if (isLocalhost()) {
+    connectStorageEmulator(firebaseStorage, "localhost", 9199);
+  }
   return firebaseStorage;
 };
-
 
 const initializeAnalytics = (app) => {
   getAnalytics(app);
@@ -42,8 +39,8 @@ const initialize = () => {
   const firebaseStorage = initializeFirebaseStorage(app);
   return {
     firestore,
-    firebaseStorage
-  }
-}
+    firebaseStorage,
+  };
+};
 
 export default initialize();
